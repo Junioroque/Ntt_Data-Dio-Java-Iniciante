55e8f42917c1ae63e6da47d75d53c7a64b1446f6
Junioroque <jcjunioroque@gmail.com>
2025-08-18 22:51:36 -0300

    Digitando o conteudo do exercicio

--- src/estrutura_dados/exercicios/arte_Abstracao_Encapsulamento/exercicio_01.java
+++
@@ -1,5 +0,0 @@
-package estrutura_dados.exercicios.arte_Abstracao_Encapsulamento;
-
-public class exercicio_01 {
-
-}
---
+++ src/estrutura_dados/exercicios/arte_Abstracao_Encapsulamento/pasta_01/Exercicio_01.java
@@ -0,0 +1,5 @@
+package estrutura_dados.exercicios.arte_Abstracao_Encapsulamento.pasta_01;
+
+public class Exercicio_01 {
+
+}
